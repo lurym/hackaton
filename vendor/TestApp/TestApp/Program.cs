bf93ccde4488536aa8746d44ea750eb0a070b102
user.name = lurym <user.email = lurym89@gmail.com>
2013-05-19 16:31:47 +0200

    Aplikacja do syncu
    Aplikacja do syncu z kalendarzem Google'a

---
+++ vendor/TestApp/TestApp/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Security.Cryptography.X509Certificates;
+using Google.Apis.Authentication.OAuth2.DotNetOpenAuth;
+using Google.Apis.Authentication.OAuth2;
+using Google.Apis.Calendar.v3;
+using DotNetOpenAuth.OAuth2;
+using Google.Apis.Services;
+using Google.Apis.Calendar.v3.Data;
+using System.Data;
+using System.Xml;
+
+
+namespace TestApp
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                Service serv = new Service();
+
+                SQLiteDatabase sqlite = new SQLiteDatabase();
+                DataTable events;
+                String query = @"select events.start, events.end, events.notes, user_event_associations.user_id, user_event_associations.state, users.email, users.name, users.surname from events, user_event_associations, users  where events.id = user_event_associations.event_id AND users.id = user_event_associations.user_id";
+                events = sqlite.GetDataTable(query);
+                foreach (DataRow dr in events.Rows)
+                {
+
+                    DateTime start = (DateTime) dr["start"];
+                    DateTime end = (DateTime) dr["end"];
+                    string notes = dr["notes"] as string;
+                    string name = dr["name"] as string;
+                    string email = dr["email"] as string;
+                    string state = dr["state"] as string;
+                    Console.WriteLine("start " + start);
+                    Console.WriteLine("end " + end);
+
+                    Event newEvent = new Event()
+                    {
+                        Summary = "Fundacja Dzieci Niczyje - dyżur",
+                        Location = "ul. Katowicka 31 Warszawa",
+                        Start = new EventDateTime()
+                        {
+                            DateTime = XmlConvert.ToString(start, XmlDateTimeSerializationMode.Local),
+                        },
+                        End = new EventDateTime()
+                        {
+                            DateTime = XmlConvert.ToString(end, XmlDateTimeSerializationMode.Local),
+                        },
+                        Description = (notes ?? string.Empty) + " Status: " + state,
+                        Creator = new Event.CreatorData()
+                        {
+                            DisplayName = name,
+                            Email = email
+                        },
+                        Reminders = new Event.RemindersData()
+                        {
+                            UseDefault = false,
+                            Overrides = new List<EventReminder>()
+                        }
+                    };
+                    newEvent.Reminders.Overrides.Add(new EventReminder()
+                    {
+                        Method = "email",
+                        Minutes = 100
+                    });
+                    
+                    Event created = serv.AddEvent(newEvent);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+}
---
+++ vendor/TestApp/TestApp/Service.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Google.Apis.Calendar.v3;
+using System.Security.Cryptography.X509Certificates;
+using Google.Apis.Authentication.OAuth2.DotNetOpenAuth;
+using Google.Apis.Authentication.OAuth2;
+using Google.Apis.Services;
+using Google.Apis.Calendar.v3.Data;
+
+namespace TestApp
+{
+    public class Service
+    {
+        private const string SERVICE_ACCOUNT_EMAIL = "779125896901@developer.gserviceaccount.com";
+        private const string SERVICE_ACCOUNT_PKCS12_FILE_PATH = @"C:\Users\lrymko\Downloads\client.p12";
+        private const string CALENDAR_ID = @"idj1e369spvfbe4ae91l8r3b5k";
+        private const string API_KEY = @"idj1e369spvfbe4ae91l8r3b5k";
+
+        private CalendarService calendarService;
+        private string calendarID;
+        private Events existingEvents;
+
+        public Service()
+        {
+            calendarService = Authorize();
+            calendarID = GetCalendarId();
+
+            existingEvents = GetEvents();
+        }
+
+        public static CalendarService Authorize()
+        {
+            X509Certificate2 certificate = new X509Certificate2(SERVICE_ACCOUNT_PKCS12_FILE_PATH, "notasecret",
+                    X509KeyStorageFlags.Exportable);
+
+
+            AssertionFlowClient provider = new AssertionFlowClient(GoogleAuthenticationServer.Description, certificate)
+            {
+                ServiceAccountId = SERVICE_ACCOUNT_EMAIL,
+                Scope = "https://www.googleapis.com/auth/calendar", //CalendarService.Scopes.Calendar.ToString()
+            };
+
+            var auth = new OAuth2Authenticator<AssertionFlowClient>(provider, AssertionFlowClient.GetState);
+
+            BaseClientService.Initializer init = new BaseClientService.Initializer();
+            init.ApiKey = API_KEY;
+            init.Authenticator = auth;
+            CalendarService service = new CalendarService(init);
+            return service;
+        }
+
+        private string GetCalendarId()
+        {
+            Google.Apis.Calendar.v3.CalendarListResource.ListRequest clrq = calendarService.CalendarList.List();
+            var result = clrq.Fetch();
+            return result.Items.First(x => x.Id.Contains(CALENDAR_ID)).Id;
+        }
+
+        public Event AddEvent(Event ev)
+        {
+            return AddOnlyIfExists(ev);
+        }
+
+        private Event AddOnlyIfExists(Event ev)
+        {
+            Event existing = FindExisting(ev);
+            if (existing == null)
+            {
+                return calendarService.Events.Insert(ev, calendarID).Fetch();
+            }
+            return existing;
+        }
+
+        private Event FindExisting(Event ev)
+        {
+            try
+            {
+                return existingEvents.Items.First(x => x.Start.DateTime.Equals(ev.Start.DateTime));
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        private Events GetEvents()
+        {
+            return calendarService.Events.List(calendarID).Fetch();
+        }
+    }
+}
